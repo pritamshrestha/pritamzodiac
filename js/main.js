87d1b12da0dc7d9fb8ebfe88e76224d0e479b25f
pritam shrestha <safal_pritam@hotmail.com>
2016-09-08 20:17:58 -0700

    first commit

--- js/main.js
+++ js/main.js
@@ -1,124 +1,60 @@
-var myfamily= [
+var myplanet= [
 { 
-  name:"prisumsa",
-  age:"hey i'm 4 years old,still baby",
-  image:"img/prisumsa.jpg"
+  name:"mercury",
+  distance:"this is the first nearest planet from the sun",
+  image:"img/mercury.jpg"
 
 },
 {
-  name:"pritam",
-  age:"hey i'm 29 years old, still young!",
-  image:"img/pritam.jpg"
+  name:"venus",
+  distance:"This is the second nearest planet form the sun!",
+  image:"img/venus.jpg"
 },
 {
-  name:"suchana",
-  age:"hey i'm 28 years old, i am still young",
-  image:"img/suchana.jpg"
+  name:"earth",
+  distance:"This is the third nearest planet from the sun",
+  image:"img/earth.jpg"
 },
 {
-  name:"premkumari",
-  age:"hey i'm 48 years old,i am already grandmother",
-  image:"img/premkumari.jpg"
+  name:"mars",
+  distance:"This is the fourth nearest planet from the sun",
+  image:"img/mars.jpg"
 },
 {
-  name:"nabin",
-  age:"hey it's me 25 years handsome nabin",
-  image:"img/nabin.jpg"
+  name:"jupitar",
+  distance:"this is the fifth nearest planet from the sun",
+  image:"img/jupitar.jpg"
 },
 {
-  name:"bimal",
-  age:"hey it's me 37 years old bimal",
-  image:"img/bimal.jpg"
+  name:"saturn",
+  distance:"This is the sixth nearest planet from the sun",
+  image:"img/saturn.jpg"
 },
 {
-  name:"rayans",
-  age:"hey it's me 3 years old baby",
-  image:"img/rayans.jpg"
+  name:"uranus",
+  distance:"This is the seventh nearest planet from the sun",
+  image:"img/uranus.jpg"
 },
 {
-  name:"shreejita",
-  age:"hey i'm 30 years old, shreejita",
-  image:"img/shreejita.jpg"
+  name:"neptune",
+  distance:"This is the eighth nearest planet from the sun",
+  image:"img/nepturn.jpg"
 },
 {
-  name:"rajendra",
-  age:"hey i'm 39 years old rajendra",
-  image:"img/rajendra.jpg"
-},
-{
-  name:"sabina",
-  age:"hey i'm 36 years old, sabina",
-  image:"img/sabina.jpg"
-},
-{
-  name:"aayus",
-  age:"hey i'm 19 years old very handsome man",
-  image:"img/aayus.jpg"
-},
-{
-  name:"aasma",
-  age:"hey i'm 22years old,beautiful girl",
-  image:"img/aasma.jpg"
-},
-{
-  name:"dhannarayan",
-  age:"hey i'm 75 years old,i am already grandfather",
-  image:"img/dhan.jpg"
-},
-{
-  name:"laxmi",
-  age:"hey i'm 72 years old,i am already  great grandmother",
-  image:"img/laxmi.jpg"
-},
-{
-  name:"netrakumar",
-  age:"hey i'm 34 years old",
-  image:"img/netrakumar.jpg"
-},
-{
-  name:"shila",
-  age:"hey i'm 29 years old",
-  image:"img/shila.jpg"
-},
-{
-  name:"sweshna",
-  age:"hey i'm 7 years old",
-  image:"img/sweshna.jpg"
-},
-{
-  name:"nirvika",
-  age:"hey i'm 48 years old,i am already grandmother",
-  image:"img/nirvika.jpg"
-},
-{
-  name:"narayan",
-  age:"hey i'm 32 years old 'daji'",
-  image:"img/daji.jpg"
-},
-
-{
-  name:"bhim",
-  age:"hey i'm 130 years old 'kancha'",
-  image:"img/bhim.jpg"
-},
-{
-  name:"ranjit",
-  age:"hey i'm 25 years old 'kid'",
-  image:"img/ranjit.jpg"
-},
-
-
-
+  name:"pluto",
+  distance:"This is the farthest planet from the sun",
+  image:"img/pluto.jpg"
+}
 ]
 // creating the function
 function myfunction(){
 
 var name = document.getElementById('name').value.toLowerCase()
-for(i=0; i<myfamily.length; i++){
-	if(name===myfamily[i].name){
-   document.getElementById('chock').innerHTML=myfamily[i].name;
-   document.getElementById('myname').innerHTML=myfamily[i].age;
-   document.getElementById('chisapani').src=myfamily[i].image;
+for(i=0; i<myplanet.length; i++){
+	if(name===myplanet[i].name){
+   document.getElementById('chock').innerHTML=myplanet[i].name;
+   document.getElementById('myname').innerHTML=myplanet[i].distance;
+   document.getElementById('chisapani').src=myplanet[i].image;
    return;
 }
 else{
